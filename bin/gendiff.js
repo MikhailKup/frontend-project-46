903f11096d251efa8237879bed299f7dbb798803
Mikhail <mifffa.mk@gmail.com>
2023-11-25 09:35:28 +0500

    Step 2. Complete

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,16 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
+
 const program = new Command();
 
-program.name('gendiff');
-
 program
+  .name('gendiff')
+  .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
   .option('-V, --version', 'output the version number')
-  .option('-h, --help', 'output usage information');
+  .option('-f, --format <type>', 'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 
 program.parse();

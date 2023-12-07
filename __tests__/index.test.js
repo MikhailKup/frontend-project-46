4b2fd47d80a245ea5f76c08b5389458f0c45ee41
Mikhail <mifffa.mk@gmail.com>
2023-12-07 10:47:53 +0500

    Step 6. Add parse.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,15 @@
 import gendiff from '../src/index.js';
 
-test('gendiff', () => {
+test('gendiff for json', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
+  const result = gendiff(filepath1, filepath2);
+  expect(gendiff(filepath1, filepath2)).toEqual(result);
+});
+
+test('gendiff for yaml, yml', () => {
+  const filepath1 = 'file1.yml';
+  const filepath2 = 'file2.yml';
   const result = gendiff(filepath1, filepath2);
   expect(gendiff(filepath1, filepath2)).toEqual(result);
 });
---
+++ src/parse.js
@@ -0,0 +1,17 @@
+import yaml from 'js-yaml';
+
+const parsers = {
+  json: JSON.parse,
+  yaml: yaml.load,
+  yml: yaml.load,
+};
+
+const getParsedObject = (data, format) => {
+  if (!parsers[format]) {
+    throw new Error('unknow file format');
+  }
+  const outputObject = parsers[format](data);
+  return outputObject;
+};
+
+export default getParsedObject;

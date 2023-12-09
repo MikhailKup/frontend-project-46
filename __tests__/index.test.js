2b341ccf08eb838938993ca27c115744a31efc78
Mikhail <mifffa.mk@gmail.com>
2023-12-09 10:42:09 +0500

    Step 8. Complete tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,11 @@
 import gendiff from '../src/index.js';
+import { readFile } from '../src/tools.js';
 
 test('gendiff for stylish', () => {
   const filepath1 = 'file1.json';
   const filepath2 = 'file2.json';
   const result = gendiff(filepath1, filepath2, 'stylish');
-  expect(gendiff(filepath1, filepath2)).toEqual(result);
+  expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
 });
 
 test('gendiff for stylish yaml, yml', () => {
@@ -12,4 +13,11 @@ test('gendiff for stylish yaml, yml', () => {
   const filepath2 = 'file2.yaml';
   const result = gendiff(filepath1, filepath2, 'stylish');
   expect(gendiff(filepath1, filepath2)).toEqual(result);
+});
+
+test('gendiff for plain', () => {
+  const filepath1 = 'file1.json';
+  const filepath2 = 'file2.json';
+  const result = gendiff(filepath1, filepath2, 'plain');
+  expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
 });

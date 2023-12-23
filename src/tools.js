74855e5f7f38e00154583dbee2b37100d17e5ea9
MikhailKup <mifffa.mk@gmail.com>
2023-12-23 12:58:16 +0500

    Step 9. Add new SSH

--- src/tools.js
+++ src/tools.js
@@ -2,6 +2,7 @@ import {readFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 import {dirname} from 'path';
 import path from 'path';
+//? ---------------------------------------------------
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);

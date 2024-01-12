8bbd6e2986f5d3e5c0f4b69a66e2ae1a1f22181f
MikhailKup <mifffa.mk@gmail.com>
2024-01-12 09:01:26 +0500

    Step 9. Fix Eslint 3

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,7 @@
-import gendiff from '../src/index.js';
 import {readFileSync} from 'fs';
 import {fileURLToPath} from 'url';
 import path, {dirname} from 'path';
+import gendiff from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -34,32 +34,3 @@ describe('gendiff', () => {
     expect(gendiff(filepath1, filepath2, 'json')).toEqual(resultJson);
   });
 });
-
-// Last files
-// test('gendiff for stylish', () => {
-//   const filepath1 = getFixturesPath('file1.json');
-//   const filepath2 = getFixturesPath('file2.json');
-//   const result = readFile('resultstylish.txt');
-//   expect(gendiff(filepath1, filepath2, 'stylish')).toEqual(result);
-// });
-
-// test('gendiff for stylish yaml, yml', () => {
-//   const filepath1 = getFixturesPath('file1.yaml');
-//   const filepath2 = getFixturesPath('file2.yaml');
-//   const result = gendiff(filepath1, filepath2, 'stylish');
-//   expect(gendiff(filepath1, filepath2)).toEqual(result);
-// });
-
-// test('gendiff for plain', () => {
-//   const filepath1 = getFixturesPath('file1.json');
-//   const filepath2 = getFixturesPath('file2.json');
-//   const result = readFile('resultplain.txt');
-//   expect(gendiff(filepath1, filepath2, 'plain')).toEqual(result);
-// });
-
-// test('gendiff for json', () => {
-//   const filepath1 = getFixturesPath('file1.json');
-//   const filepath2 = getFixturesPath('file2.json');
-//   const result = readFile('resultjson.txt');
-//   expect(gendiff(filepath1, filepath2, 'json')).toEqual(result);
-// });

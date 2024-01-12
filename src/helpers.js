59a0bf8f4e5ec918262dfc2524d5db25dedf4e30
MikhailKup <mifffa.mk@gmail.com>
2024-01-12 16:10:39 +0500

    Step 9.Fix index.js

--- src/helpers.js
+++ src/helpers.js
@@ -1,19 +1,10 @@
 import {readFileSync} from 'fs';
 import path from 'path';
-import getParsedFile from './parse.js';
-//? ---------------------------------------------------
 
-// Получение пути
 const getFilePath = (filePath) => path.resolve(process.cwd(), filePath);
 
-// const getFileFormat = (filePath) => path.extname(filePath).slice(1);
+const getFileFormat = (filePath) => path.extname(filePath).slice(1);
 
-// const readFile = (filePath) => readFileSync(filePath, 'utf-8');
+const readFile = (filePath) => readFileSync(filePath, 'utf-8');
 
-const readFile = (filePath) => {
-  const data = readFileSync(getFilePath(filePath), 'utf-8');
-  const dataType = path.extname(filePath).slice(1);
-  return getParsedFile(data, dataType);
-};
-
-export {readFile, getFilePath};
+export {readFile, getFilePath, getFileFormat};
--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
-import {readFile, getFilePath} from './helpers.js';
-// import getParsedFile from './parse.js';
+import {readFile, getFilePath, getFileFormat} from './helpers.js';
 import compareFiles from './comparefiles.js';
 import choiceFormat from './formatters/index.js';
+import getParsedFile from './parse.js';
 
-// const gendiff = (filePath1, filePath2, format = 'stylish') => {
-//   const ext1 = getFileFormat(filePath1);
-//   const path1 = getFilePath(filePath1);
-//   const data1 = readFile(path1);
-
-//   const ext2 = getFileFormat(filePath2);
-//   const path2 = getFilePath(filePath2);
-//   const data2 = readFile(path2);
-
-//   const parseData1 = readFile(data1, ext1);
-//   const parseData2 = getParsedFile(data2, ext2);
-//   const diff = compareFiles(parseData1, parseData2);
-//   return choiceFormat(diff, format);
-// };
-
-// const gendiff = (filePath1, filePath2, format = 'stylish') => {
-//   const path1 = getFilePath(filePath1);
-//   const path2 = getFilePath(filePath2);
-
-//   const parseData1 = readFile(path1);
-//   const parseData2 = readFile(path2);
-//   const diff = compareFiles(parseData1, parseData2);
-//   return choiceFormat(diff, format);
-// };
+const getParesedData = (file) => {
+  const path = getFilePath(file);
+  const data = readFile(path);
+  const dataType = getFileFormat(file);
+  return getParsedFile(data, dataType);
+};
 
 const gendiff = (filePath1, filePath2, format = 'stylish') => {
-  const parseData1 = readFile(filePath1);
-  const parseData2 = readFile(filePath2);
+  const parseData1 = getParesedData(filePath1);
+  const parseData2 = getParesedData(filePath2);
   const diff = compareFiles(parseData1, parseData2);
   return choiceFormat(diff, format);
 };

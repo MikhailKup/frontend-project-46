ca9c6659f87a577ed56f861a2fcf4435730d3a91
MikhailKup <mifffa.mk@gmail.com>
2024-01-12 08:46:20 +0500

    Step 9. Change to LF 2

--- src/comparefiles.js
+++ src/comparefiles.js
@@ -14,11 +14,11 @@ const compareFiles = (data1, data2) => {
     const value1 = data1[key];
     const value2 = data2[key];
     if (_.has(data1, key) && !_.has(data2, key)) {
-      const result = { key, value1, status: 'removed' };
+      const result = {key, value1, status: 'removed'};
       return result;
     }
     if (!_.has(data1, key) && _.has(data2, key)) {
-      const result = { key, value2, status: 'added' };
+      const result = {key, value2, status: 'added'};
       return result;
     }
     if (_.isObject(value1) && _.isObject(value2)) {
@@ -30,7 +30,7 @@ const compareFiles = (data1, data2) => {
       return result;
     }
     if (_.isEqual(value1, value2)) {
-      const result = { key, value1, status: 'unchanged' };
+      const result = {key, value1, status: 'unchanged'};
       return result;
     }
     return {

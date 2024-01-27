dc6caf5306909b6dfed53fc54516eed7941b1cbf
MikhailKup <mifffa.mk@gmail.com>
2024-01-27 07:42:30 +0500

    Complete

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,34 +7,6 @@ const getValue = (value) => {
   return _.isObject(value) ? '[complex value]' : String(value);
 };
 
-// const formatToPlain = (data) => {
-//   const iter = (value, path) => {
-//     const result = value.flatMap((node) => {
-//       const fullPath = path === '' ? `${node.key}` : `${path}.${node.key}`;
-//       switch (node.status) {
-//         case 'nested':
-//           return iter(node.children, fullPath);
-//         case 'removed':
-//           return `Property '${fullPath}' was removed`;
-//         case 'added':
-//           return `Property '${fullPath}' was added with value: ${getValue(
-//             node.value,
-//           )}`;
-//         case 'changed':
-//           return `Property '${fullPath}' was updated. From ${getValue(
-//             node.value1,
-//           )} to ${getValue(node.value2)}`;
-//         case 'unchanged':
-//           return [];
-//         default:
-//           throw new Error(`Unknown type: ${node.status}`);
-//       }
-//     });
-//     return [...result].join('\n');
-//   };
-//   return iter(data, '');
-// };
-
 const iter = (value, path) => {
   const result = value.flatMap((node) => {
     const fullPath = path === '' ? `${node.key}` : `${path}.${node.key}`;
@@ -57,7 +29,7 @@ const iter = (value, path) => {
         throw new Error(`Unknown type: ${node.status}`);
     }
   });
-  return [...result].join('\n');
+  return result.join('\n');
 };
 
 const formatToPlain = (data) => iter(data, '');
